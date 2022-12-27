e5e4a4bf45656270ac6efd27cfe3f92b68d1a152
JarnoPersoon94 <jpl.persoon@gmail.com>
2022-12-27 20:04:03 +0100

    Script wordgen added

---
+++ WordGen.js
@@ -0,0 +1,11 @@
+// make function to assign a sign to the birthday
+// make a function to randomly select different situations
+// make a function to randomly selecty a cause
+// put function in object
+// make a function that logs the prhocy
+
+const astrologyMessage = 
+{_birthDate: "DD-MM-YYYY",
+_sign: "sign",
+_situation: "situation",
+_cause: "cause"}
